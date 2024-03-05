b13d7b61fcb95e6570c1962f9f94cb4433c9bd32
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-03-05 23:18:18 +0300

    05.03.2024-23:20

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label>+888012 3457 7894</label>
+            <i className="fa fa-envelope"></i>
+            <label>example@gmail.com</label>
+          </div>
+          <div className="right row RText">
+            <label>Theme FAQ's</label>
+            <label>Need Helps</label>
+            <span>📑</span>
+            <label htmlFor="">EN</label>
+            <span>💵</span>
+            <label htmlFor="">USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
